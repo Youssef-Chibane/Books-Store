f037e29957164fd6aad76185898de426d22f3049
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-11-11 14:08:50 +0100

    book-model&route

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = `mongodb+srv://root:root@cluster0.nxet7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0`;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,52 @@
 import express, { request, response } from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/book.model.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get(`/`, (request, response) => {
     console.log(request);
     return response.status(234).send(`Welcome to MERN stack tutorial`);
 })
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+// Route to save a new book
+
+app.post(`/books`, async (request, response) => {
+    try {
+        // Validate required fields
+        if ( !request.body.title || !request.body.author || !request.body.publishYear ) {
+            return response.status(400).send({ message: `Send all required fields: title, author, publishYear` });
+        }
+
+        // If validation passes, create the new book
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+
+        // Send response with created book
+        return response.status(201).send(book);
+
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
 });
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then( () => {
+        console.log(`App connected to database`);
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
