66bba83dcda2ebdd8b667aec0e6781579ec83854
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-11-12 03:19:05 +0100

    deleteBook-Route

--- backend/index.js
+++ backend/index.js
@@ -8,99 +8,133 @@ const app = express();
 app.use(express.json());
 
 app.get(`/`, (request, response) => {
-    console.log(request);
-    return response.status(234).send(`Welcome to MERN stack tutorial`);
-})
+  console.log(request);
+  return response.status(234).send(`Welcome to MERN stack tutorial`);
+});
 
 // Route to create a new book
 
 app.post(`/books`, async (request, response) => {
-    try {
-        // Validate required fields
-        if ( !request.body.title || !request.body.author || !request.body.publishYear ) {
-            return response.status(400).send({ message: `Send all required fields: title, author, publishYear` });
-        }
-
-        // If validation passes, create the new book
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-        const book = await Book.create(newBook);
-
-        // Send response with created book
-        return response.status(201).send(book);
-
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send({ message: error.message });
+  try {
+    // Validate required fields
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response
+        .status(400)
+        .send({
+          message: `Send all required fields: title, author, publishYear`,
+        });
     }
+
+    // If validation passes, create the new book
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+
+    // Send response with created book
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
 });
 
 // Route to get all books
 
-app.get('/books', async (request, response) => {
-    try {
-        const books = await Book.find({});
-        return response.status(200).json({
-            count: books.length,
-            data : books
-        }); // Send the books in the response
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send({ message: error.message });
-    }
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    }); // Send the books in the response
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
 });
 
 // Route to get one book by id
 
-app.get('/books/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
 
-        const book = await Book.findById(id);
-        
-        return response.status(200).json(book);
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send({ message: error.message });
-    }
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
 });
 
 // Route to update one book by id
 
-app.put('/books/:id', async (request, response) => {
-    try {
-        // Validate required fields
-        if (!request.body.title || !request.body.author || !request.body.publishYear) {
-            return response.status(400).send({ message: `Send all required fields: title, author, publishYear` });
-        }
-
-        const { id } = request.params;
-
-        const result = await Book.findByIdAndUpdate(id, request.body);
-
-        if (!result) {
-            return response.status(404).json({ message: `Book not found` });
-        }
-
-        return response.status(200).send({ message: `Book updated successfully` });
-
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send({ message: error.message });
+app.put("/books/:id", async (request, response) => {
+  try {
+    // Validate required fields
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response
+        .status(400)
+        .send({
+          message: `Send all required fields: title, author, publishYear`,
+        });
     }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: `Book not found` });
+    }
+
+    return response.status(200).send({ message: `Book updated successfully` });
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+// Route to delete book by id
+
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndDelete(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: `Book not found` });
+    }
+
+    return response.status(200).send({ message: `Book deleted successfully` });
+
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
-    .connect(mongoDBURL)
-    .then( () => {
-        console.log(`App connected to database`);
-        app.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log(error);
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log(`App connected to database`);
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
